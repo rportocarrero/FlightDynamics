763e27613f970625502ffaf50f9722e132f79b9e
Ryan Portocarrero <portocarrero.ryan@gmail.com>
2022-09-17 20:20:51 -0400

    Update test_main.cpp
    main test file.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,23 @@
+/**
+ * @file test_main.cpp
+ * @author Ryan Portocarrero (ryan.portocarrero@colorado.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 // AllTests.cpp
 #include "gtest/gtest.h"
 
+/**
+ * @brief Function to run all unit tests
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int return value
+ */
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
