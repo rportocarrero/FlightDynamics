4129d3fc97c3aa95e2f507119e4bf4d74c1f02d4
Ryan Portocarrero <portocarrero.ryan@gmail.com>
2022-09-17 20:20:26 -0400

    Update main.cpp
    added docstrings for doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
+/**
+ * @file main.cpp
+ * @author Ryan Portocarrero (ryan.portocarrero@colorado.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <iostream>
 #include "Matrix.hpp"
 
 using namespace std;
 
+/**
+ * @brief Placeholder main function
+ * 
+ * @return int 
+ */
 int main(){
     cout << "Hello World" << endl;
 }
