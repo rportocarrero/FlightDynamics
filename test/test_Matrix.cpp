67bb241f1420c9afdef39f392284d66dbbc87ee3
Ryan Portocarrero <portocarrero.ryan@gmail.com>
2022-09-17 20:21:04 -0400

    Update test_Matrix.cpp
    unit tests for matrix operations

--- test/test_Matrix.cpp
+++ test/test_Matrix.cpp
@@ -1,6 +1,7 @@
-// SubtractTest.cpp
 #include "Matrix.hpp"
 #include "gtest/gtest.h"
+
+using namespace std;
 
 TEST(MatrixTest, MatrixInitialize)
 {
@@ -24,9 +25,9 @@ TEST(MatrixTest, Add2x2Matrix){
     a.data = {{1,2},{3,4}};
     b.data = {{5,6},{7,8}};
 
-    Matrix c = addMatrix(a, b);
+    Matrix c = matrix_sum(a, b);
     
-    vector<vector<double>> expected_data = {{6,8},{10,12}};
+    vector<vector<float>> expected_data = {{6,8},{10,12}};
     ASSERT_EQ(c.data,expected_data);
 }
 
@@ -40,5 +41,134 @@ TEST(MatrixTest, AddDiffDimMatrix){
     a.data = {{1,2},{3,4}};
     b.data = {{5},{6}};
 
-    EXPECT_ANY_THROW(Matrix c = addMatrix(a, b));
+    EXPECT_ANY_THROW(Matrix c = matrix_sum(a, b));
+}
+
+TEST(MatrixTest, InitColMatrix){
+    vector<float> a = {1,2,3};
+    Matrix A = new_column_matrix(a);
+
+    vector<vector<float>> expected_data = {{1},{2},{3}};
+
+    ASSERT_EQ(A.data,expected_data);
+}
+
+TEST(MatrixTest, InitRowMatrix){
+    vector<float> a = {1,2,3};
+    Matrix A = new_row_matrix(a);
+
+    vector<vector<float>> expected_data = {{1,2,3}};
+
+    ASSERT_EQ(A.data,expected_data);
+}
+
+TEST(MatrixTest, InitSquareMatrix){
+    Matrix A = new_square_matrix(3);
+    vector<vector<float>> expected_data = {{0,0,0},{0,0,0},{0,0,0}};
+    ASSERT_EQ(A.data, expected_data);
+}
+
+TEST(MatrixTest, InitUnitMatrix){
+    Matrix A = new_unit_matrix(3);
+    vector<vector<float>> expected_data = {{1,0,0},{0,1,0},{0,0,1}};
+    ASSERT_EQ(A.data, expected_data);
+}
+
+TEST(MatrixTest, InitSkewSymmetric){
+    vector<float> input = {1,2,3};
+    Matrix A = new_skew_symmetric(input);
+    vector<vector<float>> expected_data = {{0,-3,2},{3,0,-1},{-2,1,0}};
+    ASSERT_EQ(A.data, expected_data);
+}
+
+TEST(MatrixTest, InitSkewSymmetricWrongDims){
+    vector<float> input = {1,2};
+    ASSERT_ANY_THROW(new_skew_symmetric(input));
+    input = {1,2,3,4};
+    ASSERT_ANY_THROW(new_skew_symmetric(input));
+}
+
+TEST(MatrixTest, ConstantMultiplication){
+    Matrix A;
+    A.rows = 3;
+    A.cols = 3;
+    A.is_square=1;
+    A.data = {{1,2,3},{4,5,6},{7,8,9}};
+    
+    Matrix result = matrix_const_mult(A, 2);
+    vector<vector<float>> expected_data = {{2,4,6},{8,10,12},{14,16,18}};
+    ASSERT_EQ(result.data, expected_data);
+}
+
+TEST(MatrixTest, Transpose2x2){
+    Matrix A;
+    A.rows = 2;
+    A.cols = 2;
+    A.is_square = 1;
+    A.data = {{1,2},{3,4}};
+
+    Matrix result = matrix_transpose(A);
+    vector<vector<float>> expected_data = {{1,3},{2,4}};
+    ASSERT_EQ(result.data, expected_data);
+}
+
+TEST(MatrixTest, Transpose4x2){
+    Matrix A;
+    A.rows = 4;
+    A.cols = 2;
+    A.is_square = 1;
+    A.data = {{1,2},{3,4},{5,6},{7,8}};
+
+    Matrix result = matrix_transpose(A);
+    vector<vector<float>> expected_data = {{1,3,5,7},{2,4,6,8}};
+    ASSERT_EQ(result.data, expected_data);
+}
+
+TEST(MatrixTest, product2x2){
+    Matrix A;
+    A.rows = 2;
+    A.cols = 2;
+    A.is_square = 1;
+    A.data = {{-1,4},{2,3}};
+
+    Matrix B;
+    B.rows = 2;
+    B.cols = 2;
+    B.is_square = 2;
+    B.data = {{9,-3},{6,1}};
+
+    Matrix result = matrix_product(A,B);
+    
+    vector<vector<float>> expected_data = {{15,7},{36,-3}};
+    ASSERT_EQ(result.data, expected_data);
+}
+
+TEST(MatrixTest, product2x3x4){
+    Matrix A;
+    A.rows = 2;
+    A.cols = 3;
+    A.data = {{2,1,4},{0,1,1}};
+
+    Matrix B;
+    B.rows = 3;
+    B.cols = 4;
+    B.data = {{6,3,-1,0},{1,1,0,4},{-2,5,0,2}};
+
+    Matrix result = matrix_product(A,B);
+    
+    vector<vector<float>> expected_data = {{5,27,-2,12},{-1,6,0,6}};
+    ASSERT_EQ(result.data, expected_data);
+}
+
+TEST(MatrixTest, scalarProduct){
+    vector<float> A = {3,2,-1};
+    vector<float> B = {5,5,0};
+    float value = scalar_product(A,B);
+    ASSERT_EQ(value, 25);
+}
+
+TEST(MatrixTest, scalarProductInvalidDim){
+    vector<float> A = {3,2,-1,3};
+    vector<float> B = {5,5,0};
+    EXPECT_ANY_THROW(scalar_product(A,B));
 }
